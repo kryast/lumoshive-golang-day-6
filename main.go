d8b527fb2a384fcc39c080b1471e7102271d34cb
Ahmad Syarifuddin <oden.psht@gmail.com>
2024-10-14 15:11:42 +0700

    tugas 1 day 6

--- main.go
+++ main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+var a int
+var b = 1
+
+var (
+	name string = "Lumoshive"
+	age  int    = 45
+)
+
+const e float32 = 3.14
+
+var f string
+
 func main() {
-	fmt.Println("hello world")
+	c := "Lumoshive"
+	d := 2.56
+
+	fmt.Println(a, b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(f)
 }
